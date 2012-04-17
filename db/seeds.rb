6e4c6cc7c53e72ab65d84c4ffffa9798e90ccf6c
Jose <jose.ruzafa@gmail.com>
2012-04-18 01:02:58 +0200

    modified seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # password in plain is : 123456
-user = User.create(:email => 'joe@example.com', :hashed_password => '7c4a8d09ca3762af61e59520943dc26494f8941b')
+user = User.create(:email => 'joe@example.com', :password => '123456')
+user_jhon = User.create(:email => 'jhon@example.com', :password => '654321')
+user_michelle = User.create(:email => 'michelle@example.com', :password => '876543')
 
 home = Category.create(:name => 'Home', :user_id => user.id)
 work = Category.create(:name => 'Work', :user_id => user.id)
@@ -19,7 +21,7 @@ type_monthly = Type.create(:name => 'Monthly', :user_id => user.id)
 type_yearly = Type.create(:name => 'Yearly', :user_id => user.id)
 type_once = Type.create(:name => 'Once', :user_id => user.id)
 
-Entry.create(:concept => 'Revision car', :amount => '150,6' ,:category_id =>  home.id ,:user_id => user.id, :type_id => type_once.id)
+Entry.create(:concept => 'Revision car', :amount => '150.6' ,:category_id =>  home.id ,:user_id => user.id, :type_id => type_once.id)
 Entry.create(:concept => 'Children beedroms', :amount => '1000' ,:category_id =>  work.id ,:user_id => user.id, :type_id => type_once.id)
 Entry.create(:concept => 'Coffe with Michelle', :amount => '2.5' ,:category_id =>  home.id ,:user_id => user.id, :type_id => type_once.id)
 Entry.create(:concept => 'Insurance', :amount => '150,6' ,:category_id =>  home.id ,:user_id => user.id, :type_id => type_yearly.id)
