3fb30eb52eb6063cac4e24edb13e68f00a960f87
Jose <jose.ruzafa@gmail.com>
2012-04-27 00:50:09 +0200

    modified seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,24 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-jhon = User.create(:email => 'jhon@example.com', :password => '654321')
-michelle = User.create(:email => 'michelle@example.com', :password => '876543')
-
 #home = Category.create(:name => 'Home', :user_id => user.id)
 #work = Category.create(:name => 'Work', :user_id => user.id)
 #Category.create(:name => 'Leisure', :user_id => user.id)
 #Category.create(:name => 'Get away', :user_id => user.id)
-categories = Category.create([{:name => 'Home'} , {:name => 'Work'} , {:name => 'Leisure'}])
 
-user = User.create(:email => 'joe@example.com', :password => '123456', :categories => categories)
+categories_jhon = Category.create([{:name => 'Home'}, {:name => 'Work'}, {:name => 'Leisure'}])
+types_jhon = Type.create([{:name => 'Weekly'}, {:name => 'Monthly'}, {:name => 'Yearly'}, {:name => 'Once'}])
 
-Category.create(:name => 'Leisure JHON', :user_id => jhon.id)
-Category.create(:name => 'Get away JHON', :user_id => jhon.id)
+categories_michelle = Category.create([{:name => 'Home'}, {:name => 'Work'}, {:name => 'Leisure'}])
+types_michelle = Type.create([{:name => 'Weekly'}, {:name => 'Monthly'}, {:name => 'Yearly'}, {:name => 'Once'}])
 
-type_weekly = Type.create(:name => 'Weekly', :user_id => user.id)
-type_monthly = Type.create(:name => 'Monthly', :user_id => user.id)
-type_yearly = Type.create(:name => 'Yearly', :user_id => user.id)
-type_once = Type.create(:name => 'Once', :user_id => user.id)
+# users
+jhon = User.create(:email => 'jhon@example.com', :password => '654321', :categories => categories_jhon, :types => types_jhon)
+michelle = User.create(:email => 'michelle@example.com', :password => '876543', :categories => categories_michelle, :types => types_michelle)
 
-Entry.create(:concept => 'Revision car', :amount => '150.6' ,:category_id =>  home.id ,:user_id => user.id, :type_id => type_once.id)
-Entry.create(:concept => 'Children beedroms', :amount => '1000' ,:category_id =>  work.id ,:user_id => user.id, :type_id => type_once.id)
-Entry.create(:concept => 'Coffe with Michelle', :amount => '2.5' ,:category_id =>  home.id ,:user_id => user.id, :type_id => type_once.id)
-Entry.create(:concept => 'Insurance', :amount => '150,6' ,:category_id =>  home.id ,:user_id => user.id, :type_id => type_yearly.id)
+
+
+#Entry.create(:concept => 'Revision car', :amount => '150.6' ,:category_id =>  home.id ,:user_id => user.id, :type_id => type_once.id)
+#Entry.create(:concept => 'Children beedroms', :amount => '1000' ,:category_id =>  work.id ,:user_id => user.id, :type_id => type_once.id)
+#Entry.create(:concept => 'Coffe with Michelle', :amount => '2.5' ,:category_id =>  home.id ,:user_id => user.id, :type_id => type_once.id)
+#Entry.create(:concept => 'Insurance', :amount => '150,6' ,:category_id =>  home.id ,:user_id => user.id, :type_id => type_yearly.id)
