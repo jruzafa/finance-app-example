4ac75033293f6c456f84cc6d3114f680fbe66f23
Jose Ruzafa <jose.ruzafa@gmail.com>
2012-10-27 21:00:21 +0200

    Remove action show of controller users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,28 +2,6 @@ class UsersController < ApplicationController
 
   before_filter :authenticate, :only => [:edit, :update]
 
-  # GET /users
-  # GET /users.json
-  #def index
-  #  @users = User.all
-
-  #  respond_to do |format|
-  #    format.html # index.html.erb
-  #    format.json { render json: @users }
-  #  end
-  #end
-
-  # GET /users/1
-  # GET /users/1.json
-  #def show
-  #  @user = User.find(params[:id])
-
-  #  respond_to do |format|
-  #    format.html # show.html.erb
-  #    format.json { render json: @user }
-  #  end
-  #end
-
   # GET /users/new
   # GET /users/new.json
   def new
