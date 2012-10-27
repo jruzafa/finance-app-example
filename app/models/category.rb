4b7c6631f80980cd4a2a4e316602751dd36fb245
Jose Ruzafa <jose.ruzafa@gmail.com>
2012-10-27 20:29:07 +0200

    Remove method of category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,16 +1,8 @@
 class Category < ActiveRecord::Base
- validates :name, :presence => true
- belongs_to :user
- has_many :entries
- has_many :expenses
 
-
- def self.category_user(id_category)
- 	logger.debug "#{@current_user}"
- 	return current_user
- 	#return self.where("user_id = #{current_user.id}").find(@id_category)
- 	#return true
- end
-
+validates :name, :presence => true
+belongs_to :user
+has_many :entries
+has_many :expenses
 
 end
