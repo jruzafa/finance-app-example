100fc7d412905826fed403d047edb9314e7e455f
Jose Ruzafa <jose.ruzafa@gmail.com>
2012-12-24 10:47:16 +0100

    Modify method for current_month and total

--- app/models/report.rb
+++ app/models/report.rb
@@ -6,35 +6,49 @@ class Report < ActiveRecord::Base
 	end
 
 	# return entries for current month
-	def self.month_entries(user)
+	def self.entries(user, month = false)
+		if month
+			time = current_month
+			#return Entry.where(:created_at => time..time.end_of_month, :user_id => user);
+			return Entry.where(:created_at => time.beginning_of_month..time.end_of_month, :user_id => user).order('created_at DESC');
+		else
+			# return Entry.where(:user_id => user).sum('amount');
+			return Entry.where(:user_id => user).order('created_at DESC');
 
-		time = current_month
-
-		#return Entry.where(:created_at => time..time.end_of_month, :user_id => user);
-		return Entry.where(:created_at => time.beginning_of_month..time.end_of_month, :user_id => user).order('created_at DESC');
-		#return Entry.all
+		end
 	end
 
 	# return expensesn for current month
-	def self.month_expenses(user)
+	def self.expenses(user, month = false)
+		if month
+			time = current_month
 
-		time = current_month
-
-		#return Entry.where(:created_at => time..time.end_of_month, :user_id => user);
-		return Expense.where(:created_at => time.beginning_of_month..time.end_of_month, :user_id => user).order('created_at DESC');
-		#return Entry.all
+			#return Entry.where(:created_at => time..time.end_of_month, :user_id => user);
+			return Expense.where(:created_at => time.beginning_of_month..time.end_of_month, :user_id => user).order('created_at DESC');
+		else
+			#return Expense.all
+			return Expense.where(:user_id => user).order('created_at DESC');
+		end
 	end
 
-  # return the total of amount field the entries for current month
-	def self.total_entries(user)
-		time = current_month
-		return Entry.where(:created_at => time.beginning_of_month..time.end_of_month, :user_id => user).sum('amount');
+
+	# return the total of amount field the entries for current month
+	def self.total_entries(user, month = false)
+		if month
+			time = current_month
+			return Entry.where(:created_at => time.beginning_of_month..time.end_of_month, :user_id => user).sum('amount');
+		else
+			return Entry.where(:user_id => user).sum('amount');
+		end
 	end
 
 	# return the total of amount field the expenses for current month
-  def self.total_expenses(user)
-    time = current_month
-    return Expense.where(:created_at => time.beginning_of_month..time.end_of_month, :user_id => user).sum('amount');
+  def self.total_expenses(user, month = false)
+  	if month
+  		time = current_month
+    	return Expense.where(:created_at => time.beginning_of_month..time.end_of_month, :user_id => user).sum('amount');
+  	else
+		return Expense.where(:user_id => user).sum('amount');
+  	end
   end
-
 end
