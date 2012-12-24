171c21e0906e97a0716914f3cfb68bc0749f5120
Jose Ruzafa <jose.ruzafa@gmail.com>
2012-12-24 10:48:35 +0100

    add files from new action of ReportsController

--- test/functional/reports_controller_test.rb
+++ test/functional/reports_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ReportsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get current_month" do
+    get :current_month
+    assert_response :success
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
 end
