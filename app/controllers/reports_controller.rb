efdab519311dcef062c2feb57a1acdb628af829e
Jose Ruzafa <jose.ruzafa@gmail.com>
2012-12-24 10:50:11 +0100

    Add current_month method for ReportsController

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,14 +1,13 @@
 class ReportsController < ApplicationController
 
-  before_filter :authenticate
+	before_filter :authenticate
 
 	def index
-
 		# call month entries
-		@entries = Report.month_entries(current_user.id)
+		@entries = Report.entries(current_user.id,false)
 
 		# call month expenses
-		@expenses = Report.month_expenses(current_user.id)
+		@expenses = Report.expenses(current_user.id)
 
 		# totals
 		@total_entries = Report.total_entries(current_user.id)
@@ -16,12 +15,28 @@ class ReportsController < ApplicationController
 		# totals
     	@total_expenses = Report.total_expenses(current_user.id)
 
+		respond_to do |format|
+			format.html # index.html.erb
+			format.json
+		end
+	end
+
+	def current_month
+		# call month entries
+		@entries = Report.entries(current_user.id, true)
+
+		# call month expenses
+		@expenses = Report.expenses(current_user.id, true)
+
+		# totals
+		@total_entries = Report.total_entries(current_user.id, true)
+
+		# totals
+    	@total_expenses = Report.total_expenses(current_user.id, true)
 
 		respond_to do |format|
 			format.html # index.html.erb
 			format.json
 		end
-
 	end
-
 end
